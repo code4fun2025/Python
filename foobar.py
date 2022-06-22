f57bff764ba3a43bfa465f2ca0b03acfe258bac1
code4fun2025 <50508650+code4fun2025@users.noreply.github.com>
2022-06-21 23:47:22 -0700

    Update foobar.py

--- foobar.py
+++ foobar.py
@@ -1,26 +1,27 @@
 count = 5
 num = 12
+j = 2
+while(j < 5):
+    for i in range(j, 6):
+        if(num - (1 + i + j - 2) > i):
+            print(str(j - 1), str(i), str(num - (1+i)))
+            count += 1
+        else:
+            break
+    j += 1
 
-for i in range(2, 6):
-    if(num - (1 + i) > i):
-        print("1", str(i), str(num - (1+i)))
-        count += 1
-    else:
-        break
+# for i in range(3, 6):
+#     if(num - (2 + i) > i):
+#         print("2", str(i), str(num - (1+i)))
+#         count += 1
+#     else:
+#         break
 
-
-for i in range(3, 6):
-    if(num - (2 + i) > i):
-        print("2", str(i), str(num - (1+i)))
-        count += 1
-    else:
-        break
-
-for i in range(4, 6):
-    if(num - (3 + i) > i):
-        print("3", str(i), str(num - (1+i)))
-        count += 1
-    else:
-        break
+# for i in range(4, 6):
+#     if(num - (3 + i) > i):
+#         print("3", str(i), str(num - (1+i)))
+#         count += 1
+#     else:
+#         break
 
 print(count)
